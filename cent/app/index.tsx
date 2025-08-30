8b3058798edf5bbec1111e9ce00858d79a81e5e4
Christian Carlo <galarritachristiancarlo3@gmail.com>
2025-08-30 18:26:45 +0800

    Refactor Activity component layout and navigation; remove Account component

--- cent/app/index.tsx
+++ cent/app/index.tsx
@@ -53,7 +53,8 @@ export default function Index() {
       borderWidth: 1,
       borderRadius: 8,
       backgroundColor: "#edececff",
-      borderColor: "#edececff",
+      borderColor: "#dedcdcff",
+      padding: 5,
     },
 
     navBtnContainer:{
@@ -61,7 +62,7 @@ export default function Index() {
       flexDirection: "row",
       justifyContent: "space-between",
       alignItems: "center",
-      gap: 5,
+      gap: 15,
     },
 
     content:{
@@ -71,7 +72,7 @@ export default function Index() {
     },
 
     balanceCard:{
-      width: 320,
+      width: 340,
       height: 150,
       borderRadius: 20,
       backgroundColor: "#4a90e2",
@@ -193,19 +194,15 @@ export default function Index() {
           <View style={styles.navBtnContainer}>
           <TouchableOpacity onPress={() => setActiveTab("overview")} style={{ padding: 10, borderRadius: 8, flexDirection: "row", gap: 2, justifyContent: "center", alignItems: "center", backgroundColor: activeTab === "overview" ? "#ffffffff" : "#edececff" }}>
             <ChartNoAxesCombined size={15}/>
-            <Link href="/" style={{fontSize: 12}}>Overview</Link>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setActiveTab("transactions")} style={{ padding: 10, borderRadius: 8, flexDirection: "row", gap: 2, justifyContent: "center", alignItems: "center", backgroundColor: activeTab === "transactions" ? "#ffffffff" : "#edececff" }}>
-            <WalletCards size={15}/>
-            <Link href="/account" style={{fontSize: 12,}}>Accounts</Link>
+            <Link href="/" style={{fontSize: 14}}>Overview</Link>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => setActiveTab("activity")} style={{ padding: 10, borderRadius: 8,flexDirection: "row", gap: 2, justifyContent: "center", alignItems: "center", backgroundColor: activeTab === "activity" ? "#ffffffff" : "#edececff" }}>
             <Landmark size={15}/>
-            <Link href="/activity" style={{fontSize: 12}}>Activity</Link>
+            <Link href="/activity" style={{fontSize: 14}}>Activity</Link>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => setActiveTab("budget")} style={{ padding: 10, borderRadius: 8,flexDirection: "row", gap: 2, justifyContent: "center", alignItems: "center", backgroundColor: activeTab === "budget" ? "#ffffffff" : "#edececff" }}>
             <ChartPie size={15}/>
-            <Link href="/budget" style={{fontSize: 12}}>Budget</Link>
+            <Link href="/budget" style={{fontSize: 14}}>Budget</Link>
           </TouchableOpacity>
           </View>
         </View>
@@ -248,6 +245,7 @@ export default function Index() {
         </View>
       </View>
       {/* Quick Actions Section*/}
+      
       <View>
 
       </View>
